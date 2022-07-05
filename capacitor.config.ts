543e0505d94e4e252e3d09b4a997d32031776ae0
ialexss <alexandersanchezsalces@gmail.com>
2022-07-05 17:16:03 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'h21cafe-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
