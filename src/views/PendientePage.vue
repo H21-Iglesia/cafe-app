df4dfdbb507b79da6d3ef84cba60829f42e4e231
ialexss <alexandersanchezsalces@gmail.com>
2023-05-12 18:19:33 -0400

    Pestaña en pendientes hoy y deudas

--- src/views/PendientePage.vue
+++ src/views/PendientePage.vue
@@ -53,9 +53,6 @@
               
               <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index" :disabled="true">
                 <ion-item color="medium" v-if="detalle.completado">
-                  <ion-checkbox color="warning"  :checked="detalle.completado" :disabled="true">
-                  </ion-checkbox>
-
                   <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
                   <!-- <ion-select :selectedText=detalle.preparo :disabled="true">
                   </ion-select> -->
@@ -81,13 +78,119 @@
           </ion-accordion>
         </ion-accordion-group>
 
+        <!--  Todos completados hoy dia -->
+        <ion-accordion-group v-for="(Pedido, index) in pedidos" :key="index">
+          <ion-accordion v-if="pestaña == 2 ? Pedido.estado_id == 2  && ((hoy.toLocaleDateString()) == convertirFecha(Pedido.created_at).toLocaleDateString()): false">
+
+            <ion-item slot="header" value="first" color="light">
+              <ion-card-header>        
+                <ion-card-title color="dark">#{{Pedido.id + " - "+ (Pedido.nombre_cliente? Pedido.nombre_cliente:"Pedido") }} <ion-badge color="danger" v-show="!Pedido.pagado" >DEUDA</ion-badge> </ion-card-title>                       
+              </ion-card-header>
+            </ion-item>
+
+            <ion-card-content slot="content" mode="md">
+              <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index">
+                <ion-item-options side="end">
+                  <ion-item-option @click="(detalle.completado = true); GuardarProducto(detalle);">LISTO
+                  </ion-item-option>
+                </ion-item-options>
+
+                <ion-item v-show="detalle.completado == false">
+                  <ion-checkbox color="warning" slot="start" :checked="detalle.completado"
+                    @click="(detalle.completado = true); GuardarProducto(detalle);"></ion-checkbox>
+
+                  <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+
+                </ion-item>
+
+              </ion-item-sliding>
+              
+              <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index" :disabled="true">
+                <ion-item color="medium" v-if="detalle.completado">
+                  <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+                </ion-item>
+              </ion-item-sliding>                        
+              <br>
+              <ion-button color="danger" expand="block" v-if="Pedido.pagado == 0" @click="Pedido.pagado = 1; GuardarPedido(Pedido);">
+                PAGAR DEUDA
+              </ion-button>
+              <ion-button color="warning" expand="block" v-if="Pedido.estado_id == 1" @click="Pedido.estado_id = 2; GuardarPedido(Pedido); publishSocket(Pedido.id);">
+                PEDIDO COMPLETADO
+              </ion-button>
+              <ion-grid>
+                <ion-row class="ion-text-center">
+                  <ion-col>
+                    <ion-label> {{convertirFecha(Pedido.created_at).toLocaleDateString('es-ES', {day: 'numeric', month: 'long'})}}</ion-label> -
+                    <ion-label> subtotal: {{ subTotal(Pedido.pedido_detalle) }} bs</ion-label>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+
+          </ion-accordion>
+        </ion-accordion-group>
+
+        <!--  Todos Deuda -->
+        <div v-if="pestaña == 3">
+        <ion-accordion-group v-for="(Pedido, index) in pedidos" :key="index">
+          <ion-accordion v-if=" Pedido.pagado == 0">
+
+            <ion-item slot="header" value="first" color="light">
+              <ion-card-header>        
+                <ion-card-title color="dark">#{{Pedido.id + " - "+ (Pedido.nombre_cliente? Pedido.nombre_cliente:"Pedido") }} <ion-badge color="danger" v-show="!Pedido.pagado" >DEUDA</ion-badge> </ion-card-title>                       
+              </ion-card-header>
+            </ion-item>
+
+            <ion-card-content slot="content" mode="md">
+              <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index">
+                <ion-item-options side="end">
+                  <ion-item-option @click="(detalle.completado = true); GuardarProducto(detalle);">LISTO
+                  </ion-item-option>
+                </ion-item-options>
+
+                <ion-item v-show="detalle.completado == false">
+                  <ion-checkbox color="warning" slot="start" :checked="detalle.completado"
+                    @click="(detalle.completado = true); GuardarProducto(detalle);"></ion-checkbox>
+
+                  <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+
+                </ion-item>
+
+              </ion-item-sliding>
+              
+              <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index" :disabled="true">
+                <ion-item color="medium" v-if="detalle.completado">
+                  <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+                </ion-item>
+              </ion-item-sliding>                        
+              <br>
+              <ion-button color="danger" expand="block" v-if="Pedido.pagado == 0" @click="Pedido.pagado = 1; GuardarPedido(Pedido);">
+                PAGAR DEUDA
+              </ion-button>
+              <ion-button color="warning" expand="block" v-if="Pedido.estado_id == 1" @click="Pedido.estado_id = 2; GuardarPedido(Pedido); publishSocket(Pedido.id);">
+                PEDIDO COMPLETADO
+              </ion-button>
+              <ion-grid>
+                <ion-row class="ion-text-center">
+                  <ion-col>
+                    <ion-label> {{convertirFecha(Pedido.created_at).toLocaleDateString('es-ES', {day: 'numeric', month: 'long'})}}</ion-label> -
+                    <ion-label> subtotal: {{ subTotal(Pedido.pedido_detalle) }} bs</ion-label>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+
+          </ion-accordion>
+        </ion-accordion-group>
+        </div>
+
         <!--  Todos por mes -->
-        <div v-if="pestaña == 2">
+        <div v-if="pestaña == 4">
           <ion-accordion-group v-for="(Mes, index) in pedidosPorMes" :key="index">
             <ion-accordion>
               <!-- Header-->
               <ion-item slot="header" color="warning">
-                <ion-label style="font-size: 20px; margin: 10px;" >{{Mes.mes}}</ion-label>
+                <ion-label style="font-size: 20px; margin: 10px;" >{{ConvertirMesAnio(Mes.mes)}}</ion-label>
               </ion-item>
               <!-- Contenido-->
               <ion-accordion-group slot="content" v-for="(Pedido, index) in Mes.pedidos" :key="index">         
@@ -109,18 +212,14 @@
                       <ion-item v-show="detalle.completado == false">
                         <ion-checkbox color="warning" slot="start" :checked="detalle.completado"
                           @click="(detalle.completado = true); GuardarProducto(detalle);"></ion-checkbox>
-
-                        <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+                        <ion-label class="ion-text-wrap"> {{detalle.producto.nombre }}</ion-label>
                       </ion-item>
 
                     </ion-item-sliding>
                     
                     <ion-item-sliding v-for="(detalle, index) in Pedido.pedido_detalle " :key="index" :disabled="true">
-                      <ion-item color="medium" v-if="detalle.completado">
-                        <ion-checkbox color="warning"  :checked="detalle.completado" :disabled="true">
-                        </ion-checkbox>
-
-                        <ion-label class="ion-text-wrap">{{detalle.producto.nombre }}</ion-label>
+                      <ion-item color="medium" v-if="detalle.completado">                 
+                        <ion-label class="ion-text-wrap"> {{ detalle.producto.nombre }}</ion-label>
                       </ion-item>
                     </ion-item-sliding>                        
                     <br>
@@ -139,24 +238,27 @@
 
                 </ion-accordion>
               </ion-accordion-group>
-            </ion-accordion>  
-            
+            </ion-accordion>              
           </ion-accordion-group>
-        </div>
-        
-                 
+        </div>                     
     
       </ion-card>
-      
-
+            
     </ion-content>
+
     <ion-footer mode="md">
       <ion-toolbar color="light">
         <ion-segment value="default" color="warning">
           <ion-segment-button value="default" @click="pestaña = 1">
             <ion-label>Pendientes</ion-label>
           </ion-segment-button>
-          <ion-segment-button value="segment" @click="pestaña = 2">
+          <ion-segment-button value="hoy" @click="pestaña = 2">
+            <ion-label>Hoy</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="Deudas" @click="pestaña = 3">
+            <ion-label>Deudas</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="todos" @click="pestaña = 4">
             <ion-label>Todos</ion-label>
           </ion-segment-button>
         </ion-segment>
@@ -170,7 +272,6 @@ import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonItem, IonLabel, IonCard, IonCardContent, IonCardHeader,
   IonCardTitle, IonItemOption, IonItemOptions, IonItemSliding, IonRefresher, IonRefresherContent, IonAccordion,
   IonAccordionGroup, IonCheckbox, IonSegment, IonSegmentButton,IonFooter,toastController,IonBadge,IonGrid,IonRow,IonCol
-  //IonSelect, IonSelectOption,
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { ApiService } from '@/data/Services/ApiService';
@@ -208,19 +309,20 @@ export default defineComponent({
     Cargar(event: CustomEvent) {
       this.obtenerPedidosApi()
       setTimeout(() => {
+        // No borrar comentario
         // eslint-disable-next-line no-undef
         const target = event.target as HTMLIonRefresherElement;
         target.complete();
       }, 500);
     },
-    GuardarProducto(detalle) {
+    GuardarProducto(detalle:any) {
       ApiService.actualizar('pedidoProducto', detalle.id, detalle)
     },
-    GuardarPedido(pedido) {
+    GuardarPedido(pedido:any) {
       ApiService.actualizar('pedido', pedido.id, pedido)
 
     },
-    GuaradarSelectTrabajador(value, detalle) {
+    GuaradarSelectTrabajador(value:string, detalle:any) {
       detalle.preparo = value
       ApiService.actualizar('pedidoProducto', detalle.id, detalle)
     },
@@ -243,7 +345,7 @@ export default defineComponent({
         this.Notificar('bottom',message.data)
       });
     },
-    async publishSocket(numero){
+    async publishSocket(numero:string){
       const channel = ably.channels.get('pedidos');
       await channel.publish('completado', 'se completo el pedido # ' + numero);
     },
@@ -299,7 +401,16 @@ export default defineComponent({
       }, {});
 
       this.pedidosPorMes = pedidosPorMes
-      console.log(pedidosPorMes);
+    },
+    ConvertirMesAnio(fecha: string){
+      const fechaArray = fecha.split("-"); // ["5", "2023"]
+      const mes = parseInt(fechaArray[0]) - 1; // Restar 1 ya que los meses en JavaScript empiezan en 0 (enero)
+      const anio = parseInt(fechaArray[1]);
+      const fechaCompleta = new Date(anio, mes);
+      const mesTexto = fechaCompleta.toLocaleString('es-ES', { month: 'long' }); // Obtener el mes en texto completo en español
+
+      const resultado = `${mesTexto} ${anio}`; // "mayo de 2023"
+      return resultado
     }
 
 
@@ -310,7 +421,6 @@ export default defineComponent({
   components: {
     IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonItem, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItemOption, IonItemOptions, IonItemSliding
     , IonRefresher, IonRefresherContent, IonAccordion, IonAccordionGroup, IonCheckbox,IonSegment, IonSegmentButton,IonFooter,IonBadge,IonGrid,IonRow,IonCol
-    //IonSelect, IonSelectOption
   }
 });
 </script>
